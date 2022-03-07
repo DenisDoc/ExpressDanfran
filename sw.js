0d209f1e652820a8f9976628ea70188b73e6d9f3
GodILoveTequila <docxdenis@yahoo.com>
2022-03-07 01:25:06 -0800

    add to home s/fetch

--- sw.js
+++ sw.js
@@ -1,4 +1,13 @@
 "use strict";
+//install
 self.addEventListener("install", (evt) => {
   console.log("service workers have been installed");
+});
+//activate
+self.addEventListener("activate", (evt) => {
+  console.log("service worker has been activated");
+});
+//fetch
+self.addEventListener("fetch", (evt) => {
+  //   console.log("fetch event", evt);
 });
